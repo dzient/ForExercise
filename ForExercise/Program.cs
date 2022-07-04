c599c3b1e84f54b0200e79198f4c4f6ed41ba3bc
dzient <34776686+dzient@users.noreply.github.com>
2022-07-04 02:07:20 -0400

    Add files via upload
    First commit

---
+++ ForExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//-----------------------------------------------------------------
+//ForExercise
+//
+// Name: David Zientara
+// Date: 7-4-2022
+// Comments: Made a simple loop 
+// Note the $ at the beginning of the line; means the string following
+// it is an interpolated string
+//-----------------------------------------------------------------
+
+namespace ForExercise
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            for (int i = 0; i < 15; i++)
+                Console.WriteLine($"{i}");
+        }
+    }
+}
